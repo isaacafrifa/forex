f4929b83cce888d8ddd2247cbd0bed2a44153158
Afrifa <isaac.afrifa3@yahoo.com>
2022-11-08 19:48:22 +0200

    ✅ FEAT: implement config server
    - add git-config-repo for config properties files

---
+++ forex-config-server/src/main/java/iam/forexconfigserver/ForexConfigServerApplication.java
@@ -0,0 +1,15 @@
+package iam.forexconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ForexConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForexConfigServerApplication.class, args);
+    }
+
+}
