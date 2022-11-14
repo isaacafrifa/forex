14f1c7e0e15eae7000850a5fd2252ee1d326a673
Afrifa <isaac.afrifa3@yahoo.com>
2022-11-14 17:18:06 +0200

    ✅ FEAT: implement h2 database and initial inserts
    - implement jpa
    - implement initial inserts from data.sql

---
+++ exchange-rate-service/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into exchange_rate
+    (id, currency_from, currency_to, rate, environment)
+values (10001, 'USD','GHS', 14.5, '');
+insert into exchange_rate
+(id, currency_from, currency_to, rate, environment)
+values (10002, 'EUR','GHS', 15.0, '');
+insert into exchange_rate
+(id, currency_from, currency_to, rate, environment)
+values (10003, 'GBP','GHS', 17.0, '');
