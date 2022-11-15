bb8ce9c0332441ccb59d65a57c4b8df275ca974e
Afrifa <isaac.afrifa3@yahoo.com>
2022-11-15 18:05:18 +0200

    create barebones conversion service project

---
+++ conversion-service/src/main/java/iam/conversionservice/ConversionServiceApplication.java
@@ -0,0 +1,13 @@
+package iam.conversionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConversionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConversionServiceApplication.class, args);
+    }
+
+}
---
+++ conversion-service/src/test/java/iam/conversionservice/ConversionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package iam.conversionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConversionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
