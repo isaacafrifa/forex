c76ccc616d37e12ca26b8af0fa96a4bbe337dfcd
Afrifa <isaac.afrifa3@yahoo.com>
2022-11-16 20:14:37 +0200

    ✅ FEAT: implement eureka naming service

---
+++ naming-service/src/test/java/iam/namingservice/NamingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package iam.namingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
