baddcef7a765c6b09aa37bb495572d03e2f3b072
Afrifa <isaac.afrifa3@yahoo.com>
2022-11-16 20:29:51 +0200

    ✅ FEAT: connect conversion and exchange-rate services to naming server

--- conversion-service/src/main/java/iam/conversionservice/ConversionServiceApplication.java
+++ conversion-service/src/main/java/iam/conversionservice/ConversionServiceApplication.java
@@ -2,10 +2,12 @@ package iam.conversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@EnableEurekaClient
 @EnableFeignClients
+@SpringBootApplication
 public class ConversionServiceApplication {
 
     public static void main(String[] args) {
