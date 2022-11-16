e2cf331fd46c5e77804868f0162a36c93a348cce
Afrifa <isaac.afrifa3@yahoo.com>
2022-11-16 19:52:23 +0200

    ✅ FEAT: add custom configurations to feign-client
    - add custom error decoder
    - add timeouts and logger level in application properties file

--- exchange-rate-service/src/main/java/iam/exchangerateservice/controller/ExchangeRateController.java
+++ exchange-rate-service/src/main/java/iam/exchangerateservice/controller/ExchangeRateController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.math.BigDecimal;
 
 @RestController
 @RequestMapping("/api")
