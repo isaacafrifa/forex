48ddadac158dac85f4249c002128a75449c1c013
Afrifa <isaac.afrifa3@yahoo.com>
2022-11-21 16:41:43 +0200

    ✅ FEAT: implement logging filter
    - add a logging filter to log requests that are received

---
+++ api-gateway/src/main/java/iam/apigateway/filter/LoggingFilter.java
@@ -0,0 +1,19 @@
+package iam.apigateway.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class LoggingFilter implements GlobalFilter {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        log.info("Request for the path [{}] received", exchange.getRequest().getPath());
+        return chain.filter(exchange);
+    }
+}
